2770f4bec56e3545c54b3e6601d1067d35a01fa1
David Siemienas <david@siemienas.dev>
2024-08-08 02:43:53 +0000

    formatting updates

---
+++ cisco.sh
@@ -0,0 +1,1 @@
+curl -s -k -H Content-Type: application/x-www-form- xurlencoded -X POST https://10.20.244.201/api/fdm/latest/fdm/token -d grant_type=password&username=admin&password=P@ssword1! -o cisco.pw.yml
