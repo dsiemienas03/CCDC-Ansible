04b6e438f6c30aa288612f3ad5b6b82a817648d0
David Siemienas <david@siemienas.dev>
2024-09-08 19:06:22 +0000

    Fix creds

--- cisco.sh
+++ cisco.sh
@@ -1,1 +1,5 @@
-curl -s -k -H Content-Type: application/x-www-form- xurlencoded -X POST https://10.20.244.201/api/fdm/latest/fdm/token -d grant_type=password&username=admin&password=P@ssword1! -o cisco.pw.yml
+read -p "Cisco FTD IP: " ftd_ip
+read -p "Cisco FMC IP: " fmc_ip
+read -p "Cisco FTD PW: " ftd_pw
+read -p "Cisco FMC PW: " fmc_pw
+curl -s -k -H Content-Type: application/x-www-form- xurlencoded -X POST https://10.20.244.201/api/fdm/latest/fdm/token -d grant_type=password&username=admin&password=$fmc_pw -o cisco.pw.yml
